690c510d0d73d1cbffbeaeb205b923a84c178dc3
frankw <fwessels@xs4all.nl>
2017-03-30 19:30:24 -0700

    Restored original parameters for ReduceGray2x2

--- SimdSse2ReduceGray2x2_amd64.go
+++ SimdSse2ReduceGray2x2_amd64.go
@@ -6,10 +6,10 @@ package gocvsimd
 import "unsafe"
 
 //go:noescape
-func _SimdSse2ReduceGray2x2(src unsafe.Pointer, srcWidth, srcHeight, srcStride uint64, dst unsafe.Pointer, dstWidth, dstStride uint64)
+func _SimdSse2ReduceGray2x2(src unsafe.Pointer, srcWidth, srcHeight, srcStride uint64, dst unsafe.Pointer, dstWidth, dstHeight, dstStride uint64)
 
 //
 func SimdSse2ReduceGray2x2(src, dst View) {
 
-	_SimdSse2ReduceGray2x2(src.GetData(), uint64(src.GetWidth()), uint64(src.GetHeight()), uint64(src.GetStride()), dst.GetData(), uint64(dst.GetWidth()), uint64(dst.GetStride()))
+	_SimdSse2ReduceGray2x2(src.GetData(), uint64(src.GetWidth()), uint64(src.GetHeight()), uint64(src.GetStride()), dst.GetData(), uint64(dst.GetWidth()), uint64(dst.GetHeight()), uint64(dst.GetStride()))
 }

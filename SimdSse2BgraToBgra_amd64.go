1ab6d3de3d91c4b9705ad2d8c7a4848423f85df6
Harshavardhana <harsha@minio.io>
2017-03-30 19:54:45 -0700

    Implement Sse2 BgraToBgra() version.

---
+++ SimdSse2BgraToBgra_amd64.go
@@ -0,0 +1,10 @@
+package gocvsimd
+
+import "unsafe"
+
+//go:noescape
+func _SimdSse2Bgr48pToBgra32(blue unsafe.Pointer, blueStride int, width int, height int, green unsafe.Pointer, greenStride int, red unsafe.Pointer, redStride int, bgra unsafe.Pointer, bgraStride int, alpha uint8)
+
+func SimdSse2Bgr48pToBgra32(blue, green, red, bgra View, alpha uint8) {
+	_SimdSse2Bgr48pToBgra32(blue.GetData(), blue.GetStride(), blue.GetWidth(), blue.GetHeight(), green.GetData(), green.GetStride(), red.GetData(), red.GetStride(), bgra.GetData(), bgra.GetStride(), alpha)
+}

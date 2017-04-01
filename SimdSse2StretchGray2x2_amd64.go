525bc419ae690bd23f6ebee5d2aa21531efcff89
frankw <fwessels@xs4all.nl>
2017-03-31 18:45:40 -0700

    Sse2 version of StretchGray2x2

---
+++ SimdSse2StretchGray2x2_amd64.go
@@ -0,0 +1,15 @@
+//+build !noasm
+//+build !appengine
+
+package gocvsimd
+
+import "unsafe"
+
+//go:noescape
+func _SimdSse2StretchGray2x2(src unsafe.Pointer, srcWidth, srcHeight, srcStride uint64, dst unsafe.Pointer, dstWidth, dstHeight, dstStride uint64)
+
+//
+func SimdSse2StretchGray2x2(src, dst View) {
+
+	_SimdSse2StretchGray2x2(src.GetData(), uint64(src.GetWidth()), uint64(src.GetHeight()), uint64(src.GetStride()), dst.GetData(), uint64(dst.GetWidth()), uint64(dst.GetHeight()), uint64(dst.GetStride()))
+}

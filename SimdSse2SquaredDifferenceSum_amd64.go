9dc8fcb74d3534a6462bf4d5b368ed67fca0d988
frankw <fwessels@xs4all.nl>
2017-03-31 17:50:28 -0700

    Sse2 version of Squared Difference Sum

---
+++ SimdSse2SquaredDifferenceSum_amd64.go
@@ -0,0 +1,32 @@
+//+build !noasm
+//+build !appengine
+
+package gocvsimd
+
+import "unsafe"
+
+//go:noescape
+func _SimdSse2SquaredDifferenceSum(a unsafe.Pointer, aStride uint64, b unsafe.Pointer, bStride uint64, width, height uint64, sum unsafe.Pointer)
+
+//go:noescape
+func _SimdSse2SquaredDifferenceSumMasked(a unsafe.Pointer, aStride uint64, b unsafe.Pointer, bStride uint64, mask unsafe.Pointer, maskStride uint64, index uint8, width, height uint64, sum unsafe.Pointer)
+
+//
+func SimdSse2SquaredDifferenceSum(a, b View) uint64 {
+
+	sum := uint64(0)
+
+	_SimdSse2SquaredDifferenceSum(a.GetData(), uint64(a.GetStride()), b.GetData(), uint64(b.GetStride()), uint64(a.GetWidth()), uint64(a.GetHeight()), unsafe.Pointer(&sum))
+
+	return sum
+}
+
+//
+func SimdSse2SquaredDifferenceSumMasked(a, b, mask View, index uint8) uint64 {
+
+	sum := uint64(0)
+
+	_SimdSse2SquaredDifferenceSumMasked(a.GetData(), uint64(a.GetStride()), b.GetData(), uint64(b.GetStride()), mask.GetData(), uint64(mask.GetStride()), index, uint64(a.GetWidth()), uint64(a.GetHeight()), unsafe.Pointer(&sum))
+
+	return sum
+}

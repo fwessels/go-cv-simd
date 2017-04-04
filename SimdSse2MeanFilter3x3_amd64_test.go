6f40b201c36ccf44853b9ae798c200c19066802f
frankw <fwessels@xs4all.nl>
2017-04-03 18:42:05 -0700

    [WIP] Working version of Sse2 version of MeanFilter3x3 (need to optimize use of stack)

---
+++ SimdSse2MeanFilter3x3_amd64_test.go
@@ -0,0 +1,36 @@
+package gocvsimd
+
+import (
+	"testing"
+)
+
+func TestSimdSse2MeanFilter3x3(t *testing.T) {
+	grayin, grayout := View{}, View{}
+	grayin.Recreate(Resolution, Resolution, GRAY8)
+	grayout.Recreate(Resolution, Resolution, GRAY8)
+
+	src := make([]byte, grayin.GetDataLen())
+
+	for i := 0; i < grayin.GetDataLen(); i++ {
+		src[i] = byte(i << 3)
+	}
+
+	copy((*[Resolution*Resolution]byte)(grayin.GetData())[:], src[:])
+
+	SimdSse2MeanFilter3x3(grayin, grayout)
+
+	dst := make([]byte, grayout.GetDataLen())
+
+	copy(dst[:], (*[Resolution*Resolution]byte)(grayout.GetData())[:])
+
+	for r := 1; r < Resolution-1; r++ {
+		for c := 1; c < Resolution-1; c++ {
+			mean := (int(src[Resolution*(r-1)+c-1]) + int(src[Resolution*(r-1)+c]) + int(src[Resolution*(r-1)+c+1]) +
+				int(src[Resolution*r+c-1]) + int(src[Resolution*r+c]) + int(src[Resolution*r+c+1]) +
+				int(src[Resolution*(r+1)+c-1]) + int(src[Resolution*(r+1)+c]) + int(src[Resolution*(r+1)+c+1]) + 4) / 9
+			if byte(mean) != dst[Resolution+c] {
+				t.Errorf("Expected %d, got %d", mean, dst[Resolution+c])
+			}
+		}
+	}
+}

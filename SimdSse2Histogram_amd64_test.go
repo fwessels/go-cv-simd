6d1e5dea39160898fc5135f0b26d957ba140986c
frankw <fwessels@xs4all.nl>
2017-04-10 21:55:53 -0700

    Sse2 version of HistogramMasked working

--- SimdSse2Histogram_amd64_test.go
+++ SimdSse2Histogram_amd64_test.go
@@ -1,8 +1,9 @@
 package gocvsimd
 
 import (
-	"fmt"
 	"testing"
+	"time"
+	"math/rand"
 )
 
 func TestSimdSse2AbsSecondDerivativeHistogram(t *testing.T) {
@@ -13,37 +14,70 @@ func TestSimdSse2AbsSecondDerivativeHistogram(t *testing.T) {
 	src := make([]byte, grayin.GetDataLen())
 
 	for i := 0; i < grayin.GetDataLen(); i++ {
-		src[i] = byte(i)
+		src[i] = byte(i*2)
 	}
 
 	copy((*[Resolution*Resolution]byte)(grayin.GetData())[:], src[:])
 
-	SimdSse2AbsSecondDerivativeHistogram(grayin, 1, 2, histo)
+	SimdSse2AbsSecondDerivativeHistogram(grayin, 2, 4, histo)
 
-	//dst := make([]byte, histo.GetDataLen())
-	//
-	//copy(dst[:], (*[Resolution*Resolution]byte)(histo.GetData())[:])
-	//
-	//fmt.Println(dst[:64])
+	dst := make([]byte, histo.GetDataLen())
+
+	copy(dst[:], (*[Resolution*Resolution]byte)(histo.GetData())[:])
 }
 
+const maskStride = Resolution+0x40
+
 func TestSimdSse2HistogramMasked(t *testing.T) {
+
+	rand.Seed(time.Now().UTC().UnixNano())
+
 	grayin, mask := View{}, View{}
 	grayin.Recreate(Resolution, Resolution, GRAY8)
-	mask.Recreate(Resolution, Resolution, GRAY8)
+	mask.RecreateWithStride(Resolution, Resolution, maskStride, GRAY8)
 
 	src := make([]byte, grayin.GetDataLen())
 
+	h := make([]uint32, 256)
+
 	for i := 0; i < grayin.GetDataLen(); i++ {
-		src[i] = 1 + byte(i % 3)
+		src[i] = byte(rand.Intn(256))
+		h[src[i]]++
 	}
 
 	copy((*[Resolution*Resolution]byte)(grayin.GetData())[:], src[:])
 
-	histo := SimdSse2HistogramMasked(grayin, mask, 0)
+	mdata := make([]byte, mask.GetDataLen())
 
-	fmt.Println(histo[:32])
-	fmt.Println(histo[12+1*4])
-	fmt.Println(histo[12+2*4])
-	fmt.Println(histo[12+3*4])
+	maskValue := rand.Intn(256)
+
+	for i := 0; i < mask.GetDataLen(); i++ {
+		mdata[i] = byte(maskValue)
+	}
+
+	copy((*[Resolution*maskStride]byte)(mask.GetData())[:], mdata[:])
+
+	// Make mask match
+	histo := SimdSse2HistogramMasked(grayin, mask, uint64(maskValue))
+
+	for i := range histo {
+		if h[i] != histo[i] {
+			t.Errorf("For [%d]: Expected %d, got %d", i, h[i], histo[i])
+		}
+	}
+
+	// Make mask fail
+	for m := 0; m < 256; m++ {
+
+		if m == maskValue {
+			continue // skip this one
+		}
+
+		histo = SimdSse2HistogramMasked(grayin, mask, uint64(m))
+		for i := range histo {
+			if 0 != histo[i] {
+				t.Errorf("For [%d]: Expected %d, got %d", i, 0, histo[i])
+			}
+		}
+	}
 }

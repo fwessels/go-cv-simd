8cca4f6f499d29f9d3517a88189fcbc623c5fc03
frankw <fwessels@xs4all.nl>
2017-04-13 19:21:07 -0700

    Test cases for Sse2 version of Binarization

--- SimdSse2Binarization_amd64.go
+++ SimdSse2Binarization_amd64.go
@@ -6,23 +6,22 @@ package gocvsimd
 import "unsafe"
 
 //go:noescape
-func _SimdSse2AveragingBinarizationBufAllocSize(width, neighborhood uint64) int
-
-
-//go:noescape
-func _SimdSse2Binarization(src unsafe.Pointer, srcStride, width, height uint64, value, positive, negative uint8, dst unsafe.Pointer, dstStride uint64, compareType uint8)
+func _SimdSse2AveragingBinarizationBufAllocSize(width, neighborhood uint64) (alloc int)
 
 //go:noescape
-func _SimdSse2AveragingBinarization(src unsafe.Pointer, srcStride, width, height uint64, value uint8, neighborhood uint64, threshold, positive, negative uint8, dst unsafe.Pointer, dstStride uint64, compareType uint8, pbuffer unsafe.Pointer)
+func _SimdSse2Binarization(src unsafe.Pointer, srcStride, width, height uint64, value, positive, negative uint64/*uint8*/, dst unsafe.Pointer, dstStride uint64, compareType uint64/*uint8*/)
+
+//go:noescape
+func _SimdSse2AveragingBinarization(src unsafe.Pointer, srcStride, width, height uint64, value uint64/*uint8*/, neighborhood uint64, threshold, positive, negative uint64/*uint8*/, dst unsafe.Pointer, dstStride uint64, compareType uint64/*uint8*/, pbuffer unsafe.Pointer)
 
 //
-func SimdSse2Binarization(src View, value, positive, negative uint8, dst View, compareType uint8) {
+func SimdSse2Binarization(src View, value, positive, negative uint64/*uint8*/, dst View, compareType uint64/*uint8*/) {
 
 	_SimdSse2Binarization(src.GetData(), uint64(src.GetStride()), uint64(src.GetWidth()), uint64(src.GetHeight()), value, positive, negative, dst.GetData(), uint64(dst.GetStride()), compareType)
 }
 
 //
-func SimdSse2AveragingBinarization(src View, value uint8, neighborhood uint64, threshold, positive, negative uint8, dst View, compareType uint8) {
+func SimdSse2AveragingBinarization(src View, value uint64/*uint8*/, neighborhood uint64, threshold, positive, negative uint64/*uint8*/, dst View, compareType uint64/*uint8*/) {
 
 	size := _SimdSse2AveragingBinarizationBufAllocSize(uint64(src.GetWidth()), neighborhood)
 	pbuffer := Allocate(size, 32)
--- SimdSse2Binarization_amd64_test.go
+++ SimdSse2Binarization_amd64_test.go
@@ -13,12 +13,37 @@ func TestSimdSse2Binarization(t *testing.T) {
 	src := make([]byte, grayin.GetDataLen())
 
 	for i := 0; i < grayin.GetDataLen(); i++ {
-		src[i] = 128 // byte(i)
+		src[i] = byte(i)
 	}
 
 	copy((*[Resolution*Resolution]byte)(grayin.GetData())[:], src[:])
 
-	SimdSse2Binarization(grayin, 1, 192, 64, grayout, 3)
+	SimdSse2Binarization(grayin, 64, 111, 0, grayout, 3)
+
+	dst := make([]byte, grayout.GetDataLen())
+
+	copy(dst[:], (*[Resolution*Resolution]byte)(grayout.GetData())[:])
+
+	fmt.Println(dst[:Resolution])
+	fmt.Println(dst[Resolution:Resolution*2])
+	fmt.Println(dst[Resolution*2:Resolution*3])
+	fmt.Println(dst[Resolution*3:Resolution*4])
+}
+
+func TestSimdSse2AveragingBinarization(t *testing.T) {
+	grayin, grayout := View{}, View{}
+	grayin.Recreate(Resolution, Resolution, GRAY8)
+	grayout.Recreate(Resolution, Resolution, GRAY8)
+
+	src := make([]byte, grayin.GetDataLen())
+
+	for i := 0; i < grayin.GetDataLen(); i++ {
+		src[i] = byte(i)
+	}
+
+	copy((*[Resolution*Resolution]byte)(grayin.GetData())[:], src[:])
+
+	SimdSse2AveragingBinarization(grayin, 64, 3, 128, 194, 64, grayout, 3)
 
 	dst := make([]byte, grayout.GetDataLen())
 
